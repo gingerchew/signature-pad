e7472032744e016df2eaf705a5c178d1d1a35fe2
ginger <26461046+gingerchew@users.noreply.github.com>
2025-04-29 09:13:10 -0400

    Converts some static methods to functions for better minification

--- src/bezier.ts
+++ src/bezier.ts
@@ -1,5 +1,41 @@
-import { Width } from "./types";
-import { Point } from "./point";
+import type { Width, Point } from "./types";
+import { createPoint } from "./point";
+
+function calculateControlPoints(s1:Point, s2:Point, s3:Point) {
+    const dx1 = s1.x - s2.x,
+        dy1 = s1.y - s2.y,
+        dx2 = s2.x - s3.x,
+        dy2 = s2.y - s3.y,
+        m1 = { x: (s1.x + s2.x) / 2.0, y: (s1.y + s2.y) / 2.0 },
+        m2 = { x: (s2.x + s3.x) / 2.0, y: (s2.y + s3.y) / 2.0 },
+        l1 = Math.sqrt(dx1 * dx1 + dy1 * dy1),
+        l2 = Math.sqrt(dx2 * dx2 + dy2 * dy2),
+        dxm = m1.x - m2.x,
+        dym = m1.y - m2.y,
+        k = l2 / (l1 + l2),
+        cm = { x: m2.x + dxm * k, y: m2.y + dym * k },
+        tx = s2.x - cm.x,
+        ty = s2.y - cm.y;
+    return {
+        c1: createPoint(m1.x + tx, m1.y + ty),
+        c2: createPoint(m2.x + tx, m2.y + ty)
+    }
+}
+
+/* Something to do with calculating the bez curve, very magical */
+const point = (t:number, start:number, c1:number, c2:number, end:number) => (start * (1.0 - t) * (1.0 - t) * (1.0 - t))
+    + (3.0 * c1 * (1.0 - t) * (1.0 - t) * t)
+    + (3.0 * c2 * (1.0 - t) * t * t)
+    + (end * t * t * t);
+
+export const fromPoints = (points: Point[], widths: Width) => new Bezier(
+    points[1], 
+    calculateControlPoints(points[0], points[1], points[2]).c2, 
+    calculateControlPoints(points[1], points[2], points[3]).c1,
+    points[2],
+    widths.start,
+    widths.end
+)
 
 export class Bezier {
     constructor(
@@ -9,36 +45,6 @@ export class Bezier {
         public endPoint: Point, 
         public startWidth: number,
         public endWidth: number) {}
-
-    static fromPoints(
-        points:Point[], 
-        widths: Width
-    ) {
-        const c2 = this.calculateControlPoints(points[0], points[1], points[2]).c2;
-        const c3 = this.calculateControlPoints(points[1], points[2], points[3]).c1;
-        return new this(points[1], c2, c3, points[2], widths.start, widths.end);
-    }
-    
-    static calculateControlPoints(s1:Point, s2:Point, s3:Point) {
-        const dx1 = s1.x - s2.x,
-            dy1 = s1.y - s2.y,
-            dx2 = s2.x - s3.x,
-            dy2 = s2.y - s3.y,
-            m1 = { x: (s1.x + s2.x) / 2.0, y: (s1.y + s2.y) / 2.0 },
-            m2 = { x: (s2.x + s3.x) / 2.0, y: (s2.y + s3.y) / 2.0 },
-            l1 = Math.sqrt(dx1 * dx1 + dy1 * dy1),
-            l2 = Math.sqrt(dx2 * dx2 + dy2 * dy2),
-            dxm = m1.x - m2.x,
-            dym = m1.y - m2.y,
-            k = l2 / (l1 + l2),
-            cm = { x: m2.x + dxm * k, y: m2.y + dym * k },
-            tx = s2.x - cm.x,
-            ty = s2.y - cm.y;
-        return {
-            c1: new Point(m1.x + tx, m1.y + ty),
-            c2: new Point(m2.x + tx, m2.y + ty)
-        }
-    }
     
     get length() {
         const steps = 10;
@@ -46,8 +52,8 @@ export class Bezier {
         let px, py;
         for (let i = 0;i <= steps;i+=1) {
             const t = i / steps;
-            const cx = this.point(t, this.startPoint.x, this.control1.x, this.control2.x, this.endPoint.x);
-            const cy = this.point(t, this.startPoint.y, this.control1.y, this.control2.y, this.endPoint.y);
+            const cx = point(t, this.startPoint.x, this.control1.x, this.control2.x, this.endPoint.x);
+            const cy = point(t, this.startPoint.y, this.control1.y, this.control2.y, this.endPoint.y);
             if (i > 0) {
                 const xdiff = cx - px!;
                 const ydiff = cy - py!;
@@ -57,13 +63,5 @@ export class Bezier {
             py = cy;
         }
         return length;
-    }
-
-    point(t:number, start:number, c1:number, c2:number, end:number) {
-        /* Something to do with calculating the bez curve, very magical */
-        return (start * (1.0 - t) * (1.0 - t) * (1.0 - t))
-            + (3.0 * c1 * (1.0 - t) * (1.0 - t) * t)
-            + (3.0 * c2 * (1.0 - t) * t * t)
-            + (end * t * t * t);
     }
 }
