66d11d85856c05df2b02564dedfebdb9249bf9ee
ginger <26461046+gingerchew@users.noreply.github.com>
2025-04-29 09:13:40 -0400

    Converts Point class to a functional style

--- src/point.ts
+++ src/point.ts
@@ -1,7 +1,11 @@
+import type { Point } from "./types";
 
-export class Point {
-    constructor(public x: number, public y:number, public time: number = Date.now()) {}
-    distanceTo = (start: Point) => Math.sqrt(Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2));
-    equals = (other: Point) => this.x === other.x && this.y === other.y && this.time === other.time;
-    velocityFrom = (start: Point) => this.time !== start.time ? this.distanceTo(start) / (this.time - start.time) : 0;
+const distanceTo = (start:Point, end:Point) => Math.sqrt(Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2));
+const velocityFrom = (start:Point, end:Point) => end.time !== start.time ? distanceTo(start, end) / (end.time - start.time) : 0;
+const createPoint = (x:number, y:number, time: number = Date.now()) => ({ x, y, time });
+
+export {
+    distanceTo,
+    velocityFrom,
+    createPoint
 }
